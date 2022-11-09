7bea424f118f8438a956243e8f5616061546728c
prashantbharate <prshntbharte77@gmail.com>
2022-11-09 19:42:09 +0530

    Project Created

---
+++ FoodFurious/src/main/java/com/FoodFurious/demo/FoodFuriousApplication.java
@@ -0,0 +1,13 @@
+package com.FoodFurious.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFuriousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFuriousApplication.class, args);
+	}
+
+}
---
+++ FoodFurious/src/test/java/com/FoodFurious/demo/FoodFuriousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodFurious.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFuriousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
